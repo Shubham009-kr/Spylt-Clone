59aa4369a2f9af39a381664a745c8c101752527e
SlytherinKid <mr.shubhamkr2000@gmail.com>
2026-02-20 15:39:03 +0530

    added interactions to the flavours section

--- spylt-clone/src/App.jsx
+++ spylt-clone/src/App.jsx
@@ -1,23 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './components/Navbar'
 import Hero from './sections/Hero'
 import SubHero from './sections/SubHero';
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
-import { SplitText, ScrollTrigger } from 'gsap/all';
+import { SplitText, ScrollTrigger, ScrollSmoother } from 'gsap/all';
 import Flavours from './sections/Flavours';
 
 gsap.registerPlugin(useGSAP);
-gsap.registerPlugin(SplitText);
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(SplitText, ScrollTrigger, ScrollSmoother);
 
 const App = () => {
+  useGSAP(()=>{
+    ScrollSmoother.create({
+      smooth:3,
+      effects:true,
+    });
+  });
+
   return (
     <main>
       <Navbar />
-      <Hero />
-      <SubHero />
-      <Flavours />
+      <div id='smooth-wrapper'>
+        <div id='smooth-content'>
+          <Hero />
+          <SubHero />
+          <Flavours />
+        </div>
+      </div>
+      {/* <div className='w-full h-screen'></div> */}
     </main>
   )
 }
